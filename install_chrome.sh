8cc3cbf5afb28e549ae80347a23f868ff441f7cd
Mohit Dhatrak <msdhatrak25@gmail.com>
2024-02-26 19:32:01 +0530

    updated bash script

--- install_chrome.sh
+++ install_chrome.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
-sudo apt-get update
-sudo apt-get install google-chrome-stable=122.0.6261.69-1
+# Check if Google Chrome is installed
+if command -v google-chrome &> /dev/null; then
+    echo "Google Chrome is already installed."
+else
+    # Install Google Chrome
+    echo "Installing Google Chrome..."
+    wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+    dpkg -i google-chrome-stable_current_amd64.deb
+    apt-get install -f
+fi
+
