2c413aa047625f7c3ec929775f58302673e16416
Mohit Dhatrak <msdhatrak25@gmail.com>
2024-02-27 01:35:27 +0530

    bug fix

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 from flask import Flask, render_template_string, request
-from flask_cors import CORS
+from flask_cors import CORS, cross_origin
 
 from web_scrape import scrape_attendance_summary
 
 app = Flask(__name__)
-CORS(app, origins='*')
+# CORS(app, origins='*')
 # CORS(app, origins=['https://attendance-analyzer.netlify.app', 'http://localhost:5500'])
 
 @app.route('/scrape', methods=['POST'])
+@cross_origin(origins=['https://attendance-analyzer.netlify.app'])
 def scrape_data():
     req_body = request.json
     username = req_body.get('username')
@@ -18,5 +19,5 @@ def scrape_data():
     html_content = scrape_attendance_summary(username, password, months, year)
     return render_template_string('<pre>{{ html_content }}</pre>', html_content=html_content)
 
-if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+# if __name__ == '__main__':
+#     app.run(debug=True, port=5000)
