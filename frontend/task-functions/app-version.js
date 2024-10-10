2729246bc4eeedc67ad6d1441823e4cc3c04346e
Mohit Dhatrak <msdhatrak25@gmail.com>
2024-10-10 08:50:36 +0530

    clear localstorage

--- frontend/task-functions/app-version.js
+++ frontend/task-functions/app-version.js
@@ -1,4 +1,4 @@
-export const APP_VERSION = "1.0.7"; // version -> major.minor.patch
+export const APP_VERSION = "2.0.1"; // version -> major.minor.patch
 
 export const versionUpdateType = (previousVersion) => {
     const newVersion = APP_VERSION;
